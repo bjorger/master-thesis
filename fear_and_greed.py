2902073cabe19e5cf55990a122ee844f108a463c
Robin Braumann <robin.braumann@gmail.com>
2022-09-12 19:27:59 +0200

    add fear and greed index

--- fear_and_greed.py
+++ fear_and_greed.py
@@ -1,18 +1,28 @@
+from datetime import date, datetime
+from xmlrpc.client import DateTime
 from dotenv import load_dotenv
+
+from mongo_db import MongoDB
 load_dotenv()
 import os
 import requests
 from bs4 import BeautifulSoup
 
 fear_and_greed_url = os.environ.get("FEAR_AND_GREED_URL")
+fear_and_greed_collection = os.environ.get("FEAR_AND_GREED_COLLECTION")
 
 
 def fetchFearAndGreedIndex() -> None:
     resp = requests.get(fear_and_greed_url)
-    
+    mongoDb = MongoDB(fear_and_greed_collection)
+
     soup = BeautifulSoup(resp.content, 'html.parser')
     status_element = soup.find('div', 'status')
-    status_element.text
+    mongoDbObject = {
+        'created_at': datetime.now().timestamp(),
+        'fear_and_greed': status_element.text
+    }
+    mongoDb.collection.insert_one(mongoDbObject)
     
     
 fetchFearAndGreedIndex()
