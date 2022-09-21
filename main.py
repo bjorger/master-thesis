5104c83cc2b9def3625af2874b09ef8f6be29b2b
Robin Braumann <robin.braumann@gmail.com>
2022-09-21 14:53:24 +0200

    add bitcoin data

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
  
 dataCollector = DataCollector()
-dataCollector.fetchPrice(os.environ.get("BTC_TICKER"))
-"""
+
+
 schedule.every().monday.at("00:00").do(lambda: dataCollector.fetchTweets('$stx OR #stx', 'stacks'))
 schedule.every().monday.at("00:00").do(lambda: dataCollector.fetchTweets('$lrc OR #lrc', 'loopring'))    
 schedule.every(30).minutes.do(lambda: dataCollector.fetchPrice(os.environ.get("LRC_TICKER")))
@@ -19,4 +19,3 @@ schedule.every().day.at("00:00").do(lambda: dataCollector.fetchFearAndGreedIndex
 while True:
     schedule.run_pending()
     time.sleep(1)
-"""
